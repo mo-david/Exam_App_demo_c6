f937913b7f8c83559c228aa9e077a375f45ef23f
mo-david <momodawoud.2299@gmail.com>
2026-07-09 13:10:14 +0300

    files need editing

---
+++ lib/core/network/end_points.dart
@@ -0,0 +1,15 @@
+abstract class EndPoints {
+  static const String signup ="api/v1/auth/signup";
+  static const String signIn ="api/v1/auth/signin";
+  static const String changePassword = "api/v1/auth/changePassword";
+  static const String deleteMyAccount = "api/v1/auth/deleteMe";
+  static const String editProfile = "api/v1/auth/editProfile";
+  static const String logout = "api/v1/auth/logout";
+  static const String getLoggedUserInfo ="api/v1/auth/profileData";
+  static const String forgotPassword = "api/v1/auth/forgotPassword";
+  static const String verifyResetCode ="api/v1/auth/verifyResetCode";
+  static const String resetPassword = "api/v1/auth/resetPassword";
+
+
+
+}
--- lib/features/auth/data/models/source_model.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-class SourceModel extends SourceData {
-  SourceModel({required super.sourceId, required super.sourceName});
-  Factory SourceModel.fromJason (Map <String,dynamic> json){
-return SourceModel(sourceId: ['sourceId'], sourceName:['sourceName']);
-  }
-}
